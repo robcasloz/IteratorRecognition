25e366f45f68f264ce2fff4749280876b9a182a7
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-11-25 18:54:14 +0000

    factor out export functions

---
+++ include/IteratorRecognition/Exchange/JSONTransfer.hpp
@@ -0,0 +1,161 @@
+//
+//
+//
+
+#include "llvm/Analysis/LoopInfo.h"
+// using llvm::Loop
+
+#include "llvm/Support/JSON.h"
+// using json::Value
+// using json::Object
+// using json::Array
+
+#include "llvm/Support/FileSystem.h"
+// using llvm::sys::fs::F_Text
+
+#include "llvm/Support/ToolOutputFile.h"
+// using llvm::ToolOutputFile
+
+#include "llvm/Support/raw_ostream.h"
+// using llvm::raw_ostream
+// using llvm::raw_string_ostream
+
+#include "llvm/ADT/DenseSet.h"
+// using llvm::DenseSet
+
+#include "llvm/ADT/GraphTraits.h"
+// using llvm::GraphTraits
+
+#include "boost/range/adaptors.hpp"
+// using boost::adaptors::filtered
+
+#include "boost/range/algorithm.hpp"
+// using boost::range::transform
+
+#include <string>
+// using std::string
+
+#include <iterator>
+// using std::back_inserter
+
+#include <algorithm>
+// using std::transform
+
+#include <utility>
+// using std::move
+
+#include <system_error>
+// using std::error_code
+
+#ifndef ITR_JSONTRANSFER_HPP
+#define ITR_JSONTRANSFER_HPP
+
+// namspace aliases
+
+namespace ba = boost::adaptors;
+namespace br = boost::range;
+
+//
+
+namespace iteratorrecognition {
+
+auto is_not_null_unit = [](const auto &e) { return e->unit() != nullptr; };
+
+template <typename GraphT, typename GT = llvm::GraphTraits<GraphT>>
+void ExportCondensations(const GraphT &G, llvm::StringRef FilenamePart) {
+  llvm::json::Object root;
+  llvm::json::Array condensations;
+
+  for (auto &cn : G) {
+    llvm::json::Object mapping;
+    std::string outs;
+    llvm::raw_string_ostream ss(outs);
+
+    llvm::json::Array condensationsArray;
+    br::transform(cn | ba::filtered(is_not_null_unit),
+                  std::back_inserter(condensationsArray), [&](const auto &e) {
+                    ss << *e->unit();
+                    return ss.str();
+                  });
+    mapping["condensation"] = std::move(condensationsArray);
+    outs.clear();
+
+    condensations.push_back(std::move(mapping));
+  }
+
+  root["condensations"] = std::move(condensations);
+
+  std::error_code ec;
+  llvm::ToolOutputFile of("itr.scc." + FilenamePart.str() + ".json", ec,
+                          llvm::sys::fs::F_Text);
+
+  if (ec) {
+    llvm::errs() << "error opening file for writing!\n";
+    of.os().clear_error();
+  }
+
+  of.os() << llvm::formatv("{0:2}", llvm::json::Value(std::move(root)));
+  of.os().close();
+
+  if (!of.os().has_error()) {
+    of.keep();
+  }
+}
+
+template <typename NodeRef>
+void ExportCondensationToLoopMapping(
+    const llvm::DenseMap<NodeRef, llvm::DenseSet<llvm::Loop *>> &Map,
+    llvm::StringRef FilenamePart) {
+  llvm::json::Object root;
+  llvm::json::Array condensations;
+
+  for (const auto &e : Map) {
+    const auto &cn = *e.getFirst();
+    const auto &loops = e.getSecond();
+
+    llvm::json::Object mapping;
+    std::string outs;
+    llvm::raw_string_ostream ss(outs);
+
+    const auto &firstUnit = (*cn.begin())->unit();
+
+    if (firstUnit) {
+      ss << *firstUnit;
+    }
+    mapping["condensation"] = ss.str();
+    outs.clear();
+
+    llvm::json::Array loopsArray;
+    std::transform(loops.begin(), loops.end(), std::back_inserter(loopsArray),
+                   [&](const auto &e) {
+                     ss << *e->getLoopLatch()->getTerminator();
+                     return ss.str();
+                   });
+    mapping["loops"] = std::move(loopsArray);
+    outs.clear();
+
+    condensations.push_back(std::move(mapping));
+  }
+
+  root["condensations"] = std::move(condensations);
+
+  std::error_code ec;
+  llvm::ToolOutputFile of("itr.scc_to_loop." + FilenamePart.str() + ".json", ec,
+                          llvm::sys::fs::F_Text);
+
+  if (ec) {
+    llvm::errs() << "error opening file for writing!\n";
+    of.os().clear_error();
+  }
+
+  of.os() << llvm::formatv("{0:2}", llvm::json::Value(std::move(root)));
+  of.os().close();
+
+  if (!of.os().has_error()) {
+    of.keep();
+  }
+}
+
+} // namespace iteratorrecognition
+
+#endif // header
--- lib/IteratorRecognitionPass.cpp
+++ lib/IteratorRecognitionPass.cpp
@@ -10,6 +10,8 @@
 
 #include "IteratorRecognition/IteratorRecognitionPass.hpp"
 
+#include "IteratorRecognition/Exchange/JSONTransfer.hpp"
+
 #include "IteratorRecognition/CondensationGraph.hpp"
 
 #include "pedigree/Analysis/Graphs/DependenceGraphs.hpp"
@@ -47,22 +49,8 @@
 #include "llvm/ADT/DenseSet.h"
 // using llvm::DenseSet
 
-#include "llvm/ADT/iterator_range.h"
-// using llvm::make_range
-
 #include "llvm/ADT/GraphTraits.h"
 // using llvm::GraphTraits
-
-#include "llvm/Support/JSON.h"
-// using json::Value
-// using json::Object
-// using json::Array
-
-#include "llvm/Support/FileSystem.h"
-// using llvm::sys::fs::F_Text
-
-#include "llvm/Support/ToolOutputFile.h"
-// using llvm::ToolOutputFile
 
 #include "llvm/Support/CommandLine.h"
 // using llvm::cl::opt
@@ -70,10 +58,6 @@
 // using llvm::cl::location
 // using llvm::cl::cat
 // using llvm::cl::OptionCategory
-
-#include "llvm/Support/raw_ostream.h"
-// using llvm::raw_ostream
-// using llvm::raw_string_ostream
 
 #include "llvm/Support/Debug.h"
 // using DEBUG macro
@@ -83,28 +67,8 @@
 #include "boost/range/adaptors.hpp"
 // using boost::adaptors::filtered
 
-#include "boost/range/algorithm.hpp"
-// using boost::range::transform
-
-#include <string>
-// using std::string
-
-#include <algorithm>
-// using std::transform
-
-#include <iterator>
-// using std::begin
-// using std::end
-// using std::back_inserter
-
-#include <utility>
-// using std::move
-
 #include <memory>
 // using std::addressof
-
-#include <system_error>
-// using std::error_code
 
 #include <cassert>
 // using assert
@@ -116,7 +80,6 @@
 namespace itr = iteratorrecognition;
 
 namespace ba = boost::adaptors;
-namespace br = boost::range;
 
 // plugin registration for opt
 
@@ -236,8 +199,6 @@ void IteratorRecognitionPass::getAnalysisUsage(llvm::AnalysisUsage &AU) const {
   AU.setPreservesAll();
 }
 
-auto is_not_null_unit = [](const auto &e) { return e->unit() != nullptr; };
-
 template <typename GraphT, typename GT = llvm::GraphTraits<GraphT>>
 void MapCondensationToLoop(
     GraphT &G, const llvm::LoopInfo &LI,
@@ -307,101 +268,6 @@ void RecognizeIterator(
         Iterator.insert(e);
       }
     }
-  }
-}
-
-template <typename GraphT, typename GT = llvm::GraphTraits<GraphT>>
-void ExportCondensations(const GraphT &G, llvm::StringRef FilenamePart) {
-  llvm::json::Object root;
-  llvm::json::Array condensations;
-
-  for (auto &cn : G) {
-    llvm::json::Object mapping;
-    std::string outs;
-    llvm::raw_string_ostream ss(outs);
-
-    llvm::json::Array condensationsArray;
-    br::transform(cn | ba::filtered(is_not_null_unit),
-                  std::back_inserter(condensationsArray), [&](const auto &e) {
-                    ss << *e->unit();
-                    return ss.str();
-                  });
-    mapping["condensation"] = std::move(condensationsArray);
-    outs.clear();
-
-    condensations.push_back(std::move(mapping));
-  }
-
-  root["condensations"] = std::move(condensations);
-
-  std::error_code ec;
-  llvm::ToolOutputFile of("itr.scc." + FilenamePart.str() + ".json", ec,
-                          llvm::sys::fs::F_Text);
-
-  if (ec) {
-    llvm::errs() << "error opening file for writing!\n";
-    of.os().clear_error();
-  }
-
-  of.os() << llvm::formatv("{0:2}", llvm::json::Value(std::move(root)));
-  of.os().close();
-
-  if (!of.os().has_error()) {
-    of.keep();
-  }
-}
-
-template <typename NodeRef>
-void ExportCondensationToLoopMapping(
-    const llvm::DenseMap<NodeRef, llvm::DenseSet<llvm::Loop *>> &Map,
-    llvm::StringRef FilenamePart) {
-  llvm::json::Object root;
-  llvm::json::Array condensations;
-
-  for (const auto &e : Map) {
-    const auto &cn = *e.getFirst();
-    const auto &loops = e.getSecond();
-
-    llvm::json::Object mapping;
-    std::string outs;
-    llvm::raw_string_ostream ss(outs);
-
-    const auto &firstUnit = (*cn.begin())->unit();
-
-    if (firstUnit) {
-      ss << *firstUnit;
-    }
-    mapping["condensation"] = ss.str();
-    outs.clear();
-
-    llvm::json::Array loopsArray;
-    std::transform(loops.begin(), loops.end(), std::back_inserter(loopsArray),
-                   [&](const auto &e) {
-                     ss << *e->getLoopLatch()->getTerminator();
-                     return ss.str();
-                   });
-    mapping["loops"] = std::move(loopsArray);
-    outs.clear();
-
-    condensations.push_back(std::move(mapping));
-  }
-
-  root["condensations"] = std::move(condensations);
-
-  std::error_code ec;
-  llvm::ToolOutputFile of("itr.scc_to_loop." + FilenamePart.str() + ".json", ec,
-                          llvm::sys::fs::F_Text);
-
-  if (ec) {
-    llvm::errs() << "error opening file for writing!\n";
-    of.os().clear_error();
-  }
-
-  of.os() << llvm::formatv("{0:2}", llvm::json::Value(std::move(root)));
-  of.os().close();
-
-  if (!of.os().has_error()) {
-    of.keep();
   }
 }
 
