eec1a3f3292b3491e83e83d7f63efe909c611c84
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-12-07 11:49:22 +0000

    add utility lambda

--- include/IteratorRecognition/Support/Utils/Extras.hpp
+++ include/IteratorRecognition/Support/Utils/Extras.hpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 // using std::sort
 // using std::unique
+// using std::find
 
 #ifndef ITR_EXTRAS_HPP
 #define ITR_EXTRAS_HPP
@@ -22,6 +23,17 @@ auto is_not_null_unit = [](const auto &e) { return !is_null_unit(e); };
 auto unique_inplace = [](auto &Vec) {
   std::sort(Vec.begin(), Vec.end());
   Vec.erase(std::unique(Vec.begin(), Vec.end()), Vec.end());
+};
+
+auto is_subset_of = [](const auto &Set1, const auto &Set2) {
+  for (const auto &e1 : Set1) {
+    auto found = std::find(Set2.begin(), Set2.end(), e1);
+    if (found == Set2.end()) {
+      return false;
+    }
+
+    return true;
+  }
 };
 
 } // namespace iteratorrecognition
