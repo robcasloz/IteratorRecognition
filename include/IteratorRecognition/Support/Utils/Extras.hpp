c303adb74e9d993336330d17bdfe78a4ce545222
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-12-07 11:54:39 +0000

    enable ADL

--- include/IteratorRecognition/Support/Utils/Extras.hpp
+++ include/IteratorRecognition/Support/Utils/Extras.hpp
@@ -9,6 +9,10 @@
 // using std::unique
 // using std::find
 
+#include <iterator>
+// using std::begin
+// using std::end
+
 #ifndef ITR_EXTRAS_HPP
 #define ITR_EXTRAS_HPP
 
@@ -21,14 +25,20 @@ auto is_null_unit = [](const auto &e) { return e->unit() == nullptr; };
 auto is_not_null_unit = [](const auto &e) { return !is_null_unit(e); };
 
 auto unique_inplace = [](auto &Vec) {
-  std::sort(Vec.begin(), Vec.end());
-  Vec.erase(std::unique(Vec.begin(), Vec.end()), Vec.end());
+  using std::begin;
+  using std::end;
+
+  std::sort(begin(Vec), end(Vec));
+  Vec.erase(std::unique(begin(Vec), end(Vec)), end(Vec));
 };
 
 auto is_subset_of = [](const auto &Set1, const auto &Set2) {
+  using std::begin;
+  using std::end;
+
   for (const auto &e1 : Set1) {
-    auto found = std::find(Set2.begin(), Set2.end(), e1);
-    if (found == Set2.end()) {
+    auto found = std::find(begin(Set2), end(Set2), e1);
+    if (found == end(Set2)) {
       return false;
     }
 
