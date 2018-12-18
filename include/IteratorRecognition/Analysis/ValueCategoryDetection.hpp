5ec2edbde65b6373ac2979124a6cae2455f89b0d
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2018-12-18 12:01:21 +0000

    add value detection based on iterator recognition

--- include/IteratorRecognition/Analysis/ValueCategoryDetection.hpp
+++ include/IteratorRecognition/Analysis/ValueCategoryDetection.hpp
@@ -14,6 +14,8 @@
 
 #include "IteratorRecognition/Analysis/IteratorRecognition.hpp"
 
+#include "IteratorRecognition/Support/Utils/InstTraversal.hpp"
+
 namespace iteratorrecognition {
 
 void FindIteratorVars(const IteratorInfo &Info,
@@ -26,6 +28,25 @@ void FindIteratorVars(const IteratorInfo &Info,
       if (user && !Info.isIterator(user) &&
           loopBlocks.count(user->getParent())) {
         Values.insert(e);
+      }
+    }
+  }
+}
+
+void FindPayloadVars(const IteratorInfo &Info,
+                     llvm::SmallPtrSetImpl<llvm::Value *> &Values) {
+  auto &loopBlocks = Info.getLoop()->getBlocksSet();
+  auto loopInsts = make_loop_inst_range(Info.getLoop());
+
+  for (auto &e : loopInsts) {
+    if (Info.isIterator(&e)) {
+      break;
+    }
+
+    for (auto &u : e.uses()) {
+      auto *user = llvm::dyn_cast<llvm::Instruction>(u.getUser());
+      if (user && !Info.isIterator(user)) {
+        Values.insert(&e);
       }
     }
   }
