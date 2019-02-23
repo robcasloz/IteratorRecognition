f84ab32d51f8cc64a516ad2cff9ab145bebdae66
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-02-23 20:11:34 +0000

    Add new passmanager ITR pass

--- include/IteratorRecognition/Analysis/Passes/IteratorRecognitionWrapperPass.hpp
+++ include/IteratorRecognition/Analysis/Passes/IteratorRecognitionWrapperPass.hpp
@@ -11,6 +11,10 @@
 #include "llvm/Pass.h"
 // using llvm::FunctionPass
 
+#include "llvm/IR/PassManager.h"
+// using llvm::FunctionAnalysisManager
+// using llvm::AnalysisInfoMixin
+
 #include "llvm/ADT/SmallVector.h"
 // using llvm::SmallVector
 
@@ -23,8 +27,24 @@ class Loop;
 class Function;
 } // namespace llvm
 
+#define ITR_RECOGNITION_PASS_NAME "itr-recognize"
+
 namespace iteratorrecognition {
 
+// new passmanager pass
+class IteratorRecognitionAnalysis
+    : public llvm::AnalysisInfoMixin<IteratorRecognitionAnalysis> {
+  friend llvm::AnalysisInfoMixin<IteratorRecognitionAnalysis>;
+
+  static llvm::AnalysisKey Key;
+
+public:
+  using Result = IteratorRecognitionInfo;
+
+  Result run(llvm::Function &F, llvm::FunctionAnalysisManager &FAM);
+};
+
+// legacy passmanager pass
 class IteratorRecognitionWrapperPass : public llvm::FunctionPass {
   std::unique_ptr<IteratorRecognitionInfo> Info;
 
--- lib/Analysis/Passes/IteratorRecognitionWrapperPass.cpp
+++ lib/Analysis/Passes/IteratorRecognitionWrapperPass.cpp
@@ -68,7 +68,7 @@
 #include <system_error>
 // using std::error_code
 
-#define DEBUG_TYPE "itr"
+#define DEBUG_TYPE ITR_RECOGNITION_PASS_NAME
 
 // namespace aliases
 
@@ -82,8 +82,8 @@ STATISTIC(NumInseparable, "Number of inseparable top-level loops found");
 
 char itr::IteratorRecognitionWrapperPass::ID = 0;
 static llvm::RegisterPass<itr::IteratorRecognitionWrapperPass>
-    X("itr-recognize", PRJ_CMDLINE_DESC("iterator recognition pass"), false,
-      false);
+    X(ITR_RECOGNITION_PASS_NAME, PRJ_CMDLINE_DESC("iterator recognition pass"),
+      false, false);
 
 // plugin registration for clang
 
@@ -107,7 +107,58 @@ static llvm::RegisterStandardPasses RegisterIteratorRecognitionWrapperPass(
 
 //
 
+llvm::AnalysisKey itr::IteratorRecognitionAnalysis::Key;
+
 namespace iteratorrecognition {
+
+// new passmanager pass
+
+IteratorRecognitionAnalysis::Result
+IteratorRecognitionAnalysis::run(llvm::Function &F,
+                                 llvm::FunctionAnalysisManager &FAM) {
+  const auto &LI{FAM.getResult<llvm::LoopAnalysis>(F)};
+  pedigree::PDGraph &Graph{*FAM.getResult<pedigree::PDGraphAnalysis>(F)};
+  Graph.connectRootNode();
+
+#ifdef LLVM_ENABLE_STATS
+  // TODO const_cast is required due to LLVM API inconsistency with constness
+  const auto &preorderLoopForest =
+      const_cast<llvm::LoopInfo &>(LI).getLoopsInPreorder();
+  NumProcessed += preorderLoopForest.size();
+  NumTopLevelProcessed += std::distance(LI.begin(), LI.end());
+#endif // LLVM_ENABLE_STATS
+
+  // Info = std::make_unique<IteratorRecognitionInfo>(LI, Graph);
+
+  // TODO these stats need to run in the ctor
+  //#ifdef LLVM_ENABLE_STATS
+  // for (const auto &ii : Info.get()->getIteratorsInfo()) {
+  // auto numItInst = ii.getNumInstructions();
+  // auto loopInsts = make_loop_inst_range(ii.getLoop());
+  // auto numLoopInst = std::distance(loopInsts.begin(), loopInsts.end());
+
+  // if (numLoopInst == numItInst) {
+  // NumInseparable++;
+  //}
+  //}
+  //#endif // LLVM_ENABLE_STATS
+
+  // LLVM_DEBUG({
+  // for (const auto &ii : Info.get()->getIteratorsInfo()) {
+  // auto *hdr = ii.getLoop()->getHeader();
+  // llvm::dbgs() << "loop with header: " << hdr << ' ' << hdr->getName()
+  //<< '\n';
+  // llvm::dbgs() << "\titerator instructions:\n";
+  // for (auto *i : ii) {
+  // llvm::dbgs() << '\t' << *i << '\n';
+  //}
+  //}
+  //});
+
+  return {LI, Graph};
+}
+
+// legacy passmanager pass
 
 void IteratorRecognitionWrapperPass::getAnalysisUsage(
     llvm::AnalysisUsage &AU) const {
